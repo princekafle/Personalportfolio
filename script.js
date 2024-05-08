c77693790fdc1611caff7e2d65b42f5b05fde30d
Prince Kafle <89830489+princekafle@users.noreply.github.com>
2024-05-08 19:36:57 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function shownavbar() {
+    const sider = document.querySelector('.sidebar')
+    sider.style.display = 'flex'
+
+    const line =  document.querySelector('.line')
+    line.style.display='block'
+}
+
+const hidenavbar=()=> {
+    const sider = document.querySelector('.sidebar')
+    sider.style.display = 'none'
+
+    const line =  document.querySelector('.line')
+    line.style.display='none'
+}
